5070b766aaea9262152c557e5eb19104f89bb4f1
Ruy <157715108+lonelyruy@users.noreply.github.com>
2026-07-23 12:28:02 +0800

    Setup vite 3 file

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app-wrapper">
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+// Main app container
+</script>
+
+<style>
+/* Global resets */
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, sans-serif;
+  background-color: #f4f6f8;
+  color: #333;
+}
+</style>
