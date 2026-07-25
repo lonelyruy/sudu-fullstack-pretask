eea26b40dff43c2566d323d1da605c5697629a16
Ruy <157715108+lonelyruy@users.noreply.github.com>
2026-07-25 23:01:22 +0800

    Setup home page with tailwind styling and router fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,20 @@
 <template>
-  <div style="padding: 20px;">
-    <h1>Hello world</h1>
-    <p>Hello Kinomi</p>
+
+<!-- Header -->
+  <div class="min-h-screen bg-state-50 text-state-800 p-6 sm:p-10">
+    <header class = "flex flex-col sm:flex-row sm:item-center sm:justify-between gap-4 pb4 border-b border-slate-200">
+      <div>
+        <h1 class = "text-3xl font-bold  text-slate-800 tracking-tight"> Note Manager </h1>
+        <p> Origanize your toughts and tasks in one place, </p>
+      </div>
+
+      <button
+        @click="openCreateModal"
+      >
+        <span> + </span> New Note
+      </button>
+    </header>
   </div>
+
+
 </template>
